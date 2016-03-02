b748c399cd8951996c4cc796a26544fe93204ad8
William Butts <wbutts1023@gmail.com>
2016-03-01 23:57:19 -0500

    solved problem 6

--- problems_1-10.py
+++ problems_1-10.py
@@ -108,4 +108,14 @@ def divisible_check(number):
             pass
     return True
 
-problem5()
+def problem6():
+    '''The sum of the squares of the first ten natural numbers is,
+    12 + 22 + ... + 102 = 385
+    The square of the sum of the first ten natural numbers is,
+    (1 + 2 + ... + 10)2 = 552 = 3025
+    Hence the difference between the sum of the squares of the first ten
+    natural numbers and the square of the sum is 3025 − 385 = 2640.
+    Find the difference between the sum of the squares of the first
+    one hundred natural numbers and the square of the sum.'''
+    num_list = [x for x in range(1, 101)]
+    print(abs(sum([x**2 for x in num_list]) - sum(num_list)**2))
