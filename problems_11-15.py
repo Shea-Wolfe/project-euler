b313fb5008eff0a998431855e81cd58ecb3edaf2
William Butts <wbutts1023@gmail.com>
2016-03-26 19:56:15 -0400

    solved problem 14

--- problems_11-15.py
+++ problems_11-15.py
@@ -284,4 +284,34 @@ def problem13():
     number_list = number_string.split(',')
     number_list = [int(number) for number in number_list]
     print(sum(number_list))
-problem13()
+
+
+def problem14():
+    '''The following iterative sequence is defined for the set of positive integers:
+
+    n → n/2 (n is even)
+    n → 3n + 1 (n is odd)
+
+    Using the rule above and starting with 13, we generate the following sequence:
+
+    13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+    It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+    Which starting number, under one million, produces the longest chain?
+
+    NOTE: Once the chain starts the terms are allowed to go above one million.'''
+    max_length = 0
+    max_num = 0
+    for number in range(1,1000001):
+        starting_number = number
+        length = 1
+        while number != 1:
+            if number % 2 == 0:
+                number = number / 2
+            else:
+                number = 3 * number + 1
+            length += 1
+        if length > max_length:
+            max_length = length
+            max_num = starting_number
+    print("The max length is {} numbers and the number is {}".format(max_length, max_num))
